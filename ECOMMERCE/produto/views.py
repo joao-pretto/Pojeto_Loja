5dfa17f989e3b8ad739c92e553c09b59bc6ed6a4
joao-pretto <joaozz@yahoo.com.br>
2022-05-18 16:44:12 -0300

    Loja

---
+++ ECOMMERCE/produto/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views import View
+from django.http import HttpResponse
+from . import models
+
+
+class ListaProdutos(ListView):
+    model = models.Produto
+    template_name = 'produto/lista.html'
+    context_object_name = 'produtos'
+    paginate_by = 10
+
+
+class DetalheProduto(DetailView):
+
+
+class AdicionarAoCarrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Adicionar carrinho')
+
+
+class RemoverDoCarrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Remover carrinho')
+
+
+class Carrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Carrinho')
+
+
+class Finalizar(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Finalizar')
